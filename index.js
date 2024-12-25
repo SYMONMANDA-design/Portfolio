54eba6f68119cb9e60457307361454c6d5e7f61c
SYMONMANDA-design <symonmanda410@gmail.com>
2024-12-25 09:37:05 +0200

    project upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Add event listener to nav links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').slice(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
